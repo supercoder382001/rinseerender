c4f6df01c081264b6bdae5bed132ea361fa45957
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-01-25 22:24:25 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,13 @@ from app.routers import mail, map, payment,phonepe
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace "*" with specific origins for better security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Include routers
 app.include_router(mail.router, prefix="/api", tags=["Mail"])
 app.include_router(map.router, prefix="/api", tags=["Map"])
