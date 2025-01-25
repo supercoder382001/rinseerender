91e3224334bed319a74ddc47b80ddc36d05a6728
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-01-25 22:26:05 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Replace "*" with specific origins for better security
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 # Include routers
